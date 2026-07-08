ebca37557bd6f67ce70b8f379828aad4a1e79cab
Corno Schraverus <corno@schraverus.com>
2026-07-08 12:46:12 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/refiners/file_in_file_out/main.ts
+++ typescript/lib/src/interface/declarations/refiners/file_in_file_out/main.ts
@@ -7,10 +7,10 @@ import type * as d_out from "../../../data/file_to_file.js"
 import type * as d_function from "../../../data/file_to_file.js"
 import type * as d_in from "pareto-application-api/interface/data/main"
 
-export namespace interface_ {
+
     export type Parameters = p_i.Refiner<
         d_out.Parameters,
         d_function.Error,
         d_in.Parameters
     >
-}
+
--- typescript/lib/src/interface/declarations/transformers/file_in_file_out/prose.ts
+++ typescript/lib/src/interface/declarations/transformers/file_in_file_out/prose.ts
@@ -1,11 +1,11 @@
-import * as p_ from 'pareto-core/implementation/transformer'
+
 import type * as p_i from 'pareto-core/interface/transformer'
 
 //data types
 import type * as d_in from "../../../data/file_to_file.js"
 import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/prose/data"
 
-export namespace interface_ {
+
     export type Error = p_i.Transformer<
         d_in.Error,
         d_out.Phrase
@@ -14,4 +14,4 @@ export namespace interface_ {
         d_in.Command_Error,
         d_out.Phrase
     >
-}
+
