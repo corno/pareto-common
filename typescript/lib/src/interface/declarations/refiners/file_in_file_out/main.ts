74956f1c5e99f34e61cd7c705949f5bdcf9499f6
Corno Schraverus <corno@schraverus.com>
2026-07-08 12:47:30 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/refiners/file_in_file_out/main.ts
+++ typescript/lib/src/interface/declarations/refiners/file_in_file_out/main.ts
@@ -1,4 +1,4 @@
-import type * as p_i from 'pareto-core/interface/refiner'
+import type * as p_ from 'pareto-core/interface/refiner'
 import p_iterate from 'pareto-core/implementation/refiner/specials/iterate'
 
 
@@ -8,7 +8,7 @@ import type * as d_function from "../../../data/file_to_file.js"
 import type * as d_in from "pareto-application-api/interface/data/main"
 
 
-    export type Parameters = p_i.Refiner<
+    export type Parameters = p_.Refiner<
         d_out.Parameters,
         d_function.Error,
         d_in.Parameters
--- typescript/lib/src/interface/declarations/transformers/file_in_file_out/prose.ts
+++ typescript/lib/src/interface/declarations/transformers/file_in_file_out/prose.ts
@@ -1,16 +1,16 @@
 
-import type * as p_i from 'pareto-core/interface/transformer'
+import type * as p_ from 'pareto-core/interface/transformer'
 
 //data types
 import type * as d_in from "../../../data/file_to_file.js"
 import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/prose/data"
 
 
-    export type Error = p_i.Transformer<
+    export type Error = p_.Transformer<
         d_in.Error,
         d_out.Phrase
     >
-    export type Command_Error = p_i.Transformer<
+    export type Command_Error = p_.Transformer<
         d_in.Command_Error,
         d_out.Phrase
     >
