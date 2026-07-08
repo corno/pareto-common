d5de28b37e88815a4bd5cd89520b442a04976303
Corno Schraverus <corno@schraverus.com>
2026-07-08 08:32:06 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/commands/file_to_stream.ts
+++ typescript/lib/src/implementation/manual/commands/file_to_stream.ts
@@ -1,7 +1,7 @@
 import * as p_ from 'pareto-core/implementation/command'
 import p_text_from_list from 'pareto-core/implementation/transformer/specials/text_from_list'
 
-import * as interface_ from "../../../interface/declarations/commands.js"
+import type * as interface_ from "../../../interface/declarations/commands.js"
 
 //data types
 import type * as d_main from "pareto-application-api/interface/data/main"
